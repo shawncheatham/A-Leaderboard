a2eb607974c80c1306779b818d2aa1e6b70ac9a5
Shawn Cheatham <shawn00@gmail.com>
2015-02-28 17:19:00 -0500

    Format and styling Player selection re-added Bootstrap

--- leaderboardv1.js
+++ leaderboardv1.js
@@ -1,17 +1,33 @@
 // Database Collections
 PlayersList = new Meteor.Collection('players');
 
-
-
 // This is the Client 
 if(Meteor.isClient){
-  Template.leaderboard.helpers({
-    player: function(){
-      return PlayersList.find();
-    }
-  });
-}
 
+	Template.leaderboard.helpers({
+		'player': function(){
+			return PlayersList.find()
+		},
+
+		'selectedClass': function(){
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+				if(selectedPlayer === playerId){
+					return "selected"
+				}
+		}
+	});
+
+	Template.leaderboard.events({
+		'click li.player': function(){
+			var playerId = this._id;
+			Session.set('selectedPlayer', playerId);
+			var selectedPlayer = Session.get('selectedPlayer');
+			console.log(selectedPlayer);
+		}
+	});
+}
 // This is the Server
 if(Meteor.isServer){
+
 }
