f29d55df9c2a453c6ef433736b8e5b01cc101677
Shawn Cheatham <shawn00@gmail.com>
2015-02-28 14:58:26 -0500

    Showing Player, Score, Added basic formatting with Bootstrap

--- leaderboardv1.js
+++ leaderboardv1.js
@@ -1,1 +1,17 @@
+// Database Collections
 PlayersList = new Meteor.Collection('players');
+
+
+
+// This is the Client 
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    player: function(){
+      return PlayersList.find();
+    }
+  });
+}
+
+// This is the Server
+if(Meteor.isServer){
+}
