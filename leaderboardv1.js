3b54570d50fd490fe0ee69e50be2f984af2487f7
Shawn Cheatham <shawn00@gmail.com>
2015-03-01 13:40:05 -0500

    Login Accounts and cleanup

--- leaderboardv1.js
+++ leaderboardv1.js
@@ -4,11 +4,16 @@ PlayersList = new Meteor.Collection('players');
 // This is the Client 
 if(Meteor.isClient){
 
-	Template.leaderboard.helpers({
-		'player': function(){
-			return PlayersList.find({}, {sort: {score: -1, name: 1}});
-		},
+	Meteor.subscribe('thePlayers');
 
+	// Template Helpers
+	Template.leaderboard.helpers({
+		// Getting Players for User, show and sort
+		'player': function(){
+			var currentUserId = Meteor.userId();
+			return PlayersList.find({ createdBy: currentUserId }, {sort: {score: -1, name: 1}});
+		},
+		// Selecting the Player within the list
 		'selectedClass': function(){
 			var playerId = this._id;
 			var selectedPlayer = Session.get('selectedPlayer');
@@ -16,49 +21,74 @@ if(Meteor.isClient){
 					return "selected"
 				}
 		},
-
+		// Show the Player being selected from the list
 		'showSelectedPlayer' : function(){
 			var selectedPlayer = Session.get('selectedPlayer');
 			return PlayersList.findOne(selectedPlayer);
 		}
 	});
-
+	// Events related to adding new Player
 	Template.leaderboard.events({
+		
+		// Selecting the player and storing for session
 		'click li.player': function(){
 			var playerId = this._id;
 			Session.set('selectedPlayer', playerId);
 			var selectedPlayer = Session.get('selectedPlayer');
-			console.log(selectedPlayer);
 		}, 
-
+		
+		// For selected Player, add 5 points
 		'click #increment': function(){
 			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.update({_id: selectedPlayer}, {$inc: {score: 5}});
+			Meteor.call('modifiedPlayerScore', selectedPlayer, 5);
 		},
-
+		
+		// For selected Player, subtract 5 points
 		'click #subtract': function(){
 			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.update({_id: selectedPlayer}, {$inc: {score: -5}});
+			Meteor.call('modifiedPlayerScore', selectedPlayer, -5);
 		},
 
+		// Remove the selected Player
 		'click #remove': function(){
 			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.remove(selectedPlayer);
+			Meteor.call('removePlayer', selectedPlayer);
 		}
 	});
 
+	// Add Player to list for given User
 	Template.addPlayerForm.events({
 		'submit form': function(event, template){
 			event.preventDefault();
 			var playerNameVar = template.find('#playerName').value;
-			PlayersList.insert({
-				name: playerNameVar,
-				score: 0
-			});
+			Meteor.call('insertPlayerData', playerNameVar);
 		}
 	});
 }
 // This is the Server
 if(Meteor.isServer){
+	Meteor.publish('thePlayers', function(){
+		var currentUserId = this.userId;
+		return PlayersList.find({ createdBy: currentUserId });
+	});
 
+	Meteor.methods({
+		'insertPlayerData': function(playerNameVar){
+			var currentUserId = Meteor.userId();
+			PlayersList.insert({
+				name: playerNameVar,
+				score: 0,
+				createdBy: currentUserId
+			})
+			console.log("Hello World");
+		},
+
+		'removePlayer': function(selectedPlayer){
+			PlayersList.remove(selectedPlayer);
+		},
+
+		'modifiedPlayerScore': function(selectedPlayer, scoreValue){
+			PlayersList.update({_id: selectedPlayer}, {$inc: {score: scoreValue}});
+		}
+	});
 }
